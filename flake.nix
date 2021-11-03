63c521ffef59c0047d6f96e1da9cabce843551c5
lincoln auster [they/them] <lincolnauster@gmail.com>
2021-11-03 16:21:03 -0600

    package with Nix
    This is for personal use. Do not depend on this. It is ugly.
    
    I'm *definitely* concerned that I needlessly screwed with some
    dependencies, and most of the past three commits are at best going to
    cause merge conflicts down the line while providing no utility
    whatsoever. If I were smart, I'd go through and fix then rebase all
    this, but all I actually care about is using this Amp fork in my NixOS
    config.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.naersk.url = "github:nix-community/naersk";
+
+  inputs.fenix.url = "github:nix-community/fenix";
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, fenix, naersk }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+        rust_tc = fenix.packages.x86_64-linux.minimal;
+        rust = pkgs.makeRustPlatform {
+          rustc = rust_tc.rustc; cargo = rust_tc.cargo;
+        };
+    in {
+      defaultPackage.x86_64-linux = rust.buildRustPackage {
+        name = "amp";
+        version = "0.6.2-p";
+        src = ./.;
+
+        cargoSha256 = "sha256-7W/u6tbo1ovUYbqYPpGAQ/pmiPPfRxdoDuqENnE37Kw=";
+
+        nativeBuildInputs = [ pkgs.python3 ];
+        buildInputs = [ pkgs.xorg.libxcb ];
+
+        doCheck = false; # Nix has some ... issues ... with the tests.
+      };
+    };
+}
--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ extern crate signal_hook;
 extern crate syntect;
 extern crate unicode_segmentation;
 extern crate clipboard;
-extern crate yaml_rust_fork as yaml;
+extern crate yaml_rust as yaml;
 extern crate smallvec;
 
 #[macro_use]
